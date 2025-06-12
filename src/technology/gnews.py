2b16751defdefc1f03460bd9b636e40f6b67ce61
chitresh99 <chitreshpoojary990@gmail.com>
2025-06-12 18:26:27 +0530

    init

---
+++ src/technology/gnews.py
@@ -0,0 +1,55 @@
+import os
+import json
+import httpx
+import random
+import json
+import urllib.request
+from dotenv import load_dotenv
+from agno.agent import Agent
+from agno.models.google import Gemini
+from agno.tools import tool
+
+load_dotenv()
+
+gnews_api_key=os.getenv('g_news')
+google_api_key=os.getenv('google_api_key')
+
+
+def get_gnews_stories() -> str:
+
+    category = "technology"
+    url = f"https://gnews.io/api/v4/top-headlines?category={category}&lang=en&country=us&max=10&apikey={gnews_api_key}"
+
+    with urllib.request.urlopen(url) as response:
+     data = json.loads(response.read().decode("utf-8"))
+    articles = data["articles"]
+    summaries = []
+
+    # for i in range(len(articles)):
+    #     # articles[i].title
+    #     print(f"Title: {articles[i]['title']}")
+    #     # articles[i].description
+    #     print(f"Description: {articles[i]['description']}")
+    #     # You can replace {property} below with any of the article properties returned by the API.
+    #     # articles[i].{property}
+    #     print(f"articles[i].{property}")
+    #     # Delete this line to display all the articles returned by the request. Currently only the first article is displayed.
+    #     print()
+
+    for i,article in enumerate(articles):
+       summaries.append(f"{i+1}.{article['title']}: {article['description']}")
+
+    return "\n".join(summaries)
+
+gnews = get_gnews_stories()
+
+
+agent_data = Agent(
+    model=Gemini(id="gemini-2.0-flash",api_key=google_api_key),
+    tools=[gnews],
+    show_tool_calls=True,
+    markdown=True
+)
+
+agent = agent_data
+agent.print_response(f"Here are some news headlines:\n{gnews}\n\nSummarize these in bullet points.", stream=True)
---
+++ src/technology/techcrunch.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+from newsapi import NewsApiClient
+import json
+import httpx
+import random
+import os 
+from dotenv import load_dotenv
+
+from agno.agent import Agent
+from agno.models.google import Gemini
+from agno.tools import tool
+
+load_dotenv()
+news_api_key=os.getenv('News_api')
+google_api_key=os.getenv('google_api_key')
+
+newsapi = NewsApiClient(news_api_key)
+
+def get_techcrunch() -> str:
+    top_headlines = newsapi.get_top_headlines(category='technology',
+                                          language='en',
+                                          country='us')
+
+    articles = top_headlines["articles"]
+    summaries = []
+
+    for i,article in enumerate(articles):
+         summaries.append(f"{i+1}. {article['title']} - {article['description']}")
+    
+    return "\n".join(summaries)
+
+
+techcrunch = get_techcrunch()
+agent_data = Agent(
+    model=Gemini(id="gemini-2.0-flash",api_key=google_api_key),
+    markdown=True
+)
+
+agent = agent_data
+agent.print_response(f"Here are some news headlines:\n{techcrunch}\n\nSummarize these in bullet points.", stream=True)
