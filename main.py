36755c4984de0310a20be8a626a7ce2ea3aaad11
chitresh99 <chitreshpoojary990@gmail.com>
2025-06-14 17:02:39 +0530

    some changes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from src.gnews import get_gnews_stories
 from src.technews import get_technews
-from src.hacknernews import get_news
+from src.hackernews import get_news
 
 from agno.agent import Agent
 from agno.models.google import Gemini
 from agno.tools import tool
 
-import os 
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,13 +16,9 @@ gnews=get_gnews_stories()
 technews=get_technews()
 hackernews=get_news(limit=10)
 
-# print(gnews)
-# print(technews)
-# print(hackernews)
-
 agent_data = Agent(
-    model=Gemini(id="gemini-2.0-flash",api_key=google_api_key),
-    markdown=True
+        model=Gemini(id="gemini-2.0-flash",api_key=google_api_key),
+        markdown=True
 )
 agent = agent_data
 agent.print_response(f"Here are some news headlines:\n{hackernews,gnews,technews}\n\nSummarize these in bullet points.", stream=True)
